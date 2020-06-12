5555a8777c3ebe41b7a20b58a9079fc25b617192
Jason Clegg <tjason.clegg@gmail.com>
2020-06-12 12:08:25 -0600

    Need to fix a few things

---
+++ smurfs/src/components/UseInput.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const useInput = (initialValue) => {
+  const [state, setState] = useState(initialValue);
+  const handleClange = (updatedValue) => {
+    setState(updatedValue);
+  };
+  return [state, setState, handleClange];
+};
+
+export default useInput;
